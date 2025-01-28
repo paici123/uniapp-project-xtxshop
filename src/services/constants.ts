b8319507c59f074cbba03f4847f31d1152460713
paici123 <2272971255@qq.com>
2025-01-28 23:04:25 +0800

    第一次提交完成项目开发

---
+++ src/services/constants.ts
@@ -0,0 +1,19 @@
+/** 订单状态枚举 */
+export enum OrderStates {
+  DaiFuKuan = 1 /** 待付款 */,
+  DaiFaHuo = 2 /** 待发货 */,
+  DaiShouHuo = 3 /** 待收货 */,
+  DaiPingJia = 4 /** 待评价 */,
+  YiWanCheng = 5 /** 已完成 */,
+  YiQuXiao = 6 /** 已取消 */,
+}
+/** 订单状态列表 */
+export const orderStateList = [
+  { id: 0, text: '' },
+  { id: 1, text: '待付款' },
+  { id: 2, text: '待发货' },
+  { id: 3, text: '待收货' },
+  { id: 4, text: '待评价' },
+  { id: 5, text: '已完成' },
+  { id: 6, text: '已取消' },
+]
